db5d15d0eead4202aa35f1a9649bc1c208276459
Alysson Henrique <alyssonhra99@gmail.com>
2023-06-24 23:01:49 -0300

    feat: end-points valididations implemented + pagination (?page=number) to retrieve tweets + getting the username from the headers

--- src/app.js
+++ src/app.js
@@ -13,20 +13,28 @@ app.listen(PORT, () => console.log(`Server on - PORT: ${PORT}`));
 app.post("/sign-up", (req, res) => {
     const { body: { username, avatar } } = req;
     try {
+        if ((!username || !avatar)
+            || (username.toString() !== username || avatar.toString() !== avatar))
+            return res.status(400).send("Todos os campos são obrigatórios!");
+
         const newUser = { username, avatar }
         users.push(newUser);
-        res.send("OK");
+        res.status(201).send("OK");
     } catch (error) {
         res.status(500).send("Houve um problema no registro, por favor tente novamente.");
     }
 });
 
 app.post("/tweets", (req, res) => {
-    const { body: { username, tweet } } = req;
-    if (!users.find(user => user.username == username)) return res.status(401).send("UNAUTHORIZED");
+    const { tweet } = req.body;
+    const { user } = req.headers;
+    if (!users.find(p => p.username == user)) return res.status(401).send("UNAUTHORIZED");
+
+    if (!tweet || tweet.toString() !== tweet) return res.status(400).send("Todos os campos são obrigatórios!");
 
     try {
-        tweets.push({ username, tweet });
+        if (!user || !tweet) return res.status(400).send("Todos os campos são obrigatórios!");
+        tweets.unshift({ username: user, tweet });
         res.status(201).send("OK");
     } catch (error) {
         res.status(500).send("Houve um problema interno, por favor tente novamente.");
@@ -34,6 +42,18 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
+    const { page } = req.query;
+    let start = 0, end = 10;
+
+    if (page) {
+        if (page >= 1) {
+            end = page * 10;
+            start = end - 10;
+        } else {
+            res.status(400).send("Informe uma página válida!");
+        }
+    }
+
     try {
         const tweetsInfo = tweets.map(tweet => ({
             ...tweet,
@@ -41,7 +61,7 @@ app.get("/tweets", (req, res) => {
         }));
 
         if (tweetsInfo.length > 10) {
-            res.status(200).send(tweetsInfo.slice(0, 10));
+            res.status(200).send(tweetsInfo.slice(start, end));
         } else {
             res.status(200).send(tweetsInfo);
         }
@@ -50,3 +70,18 @@ app.get("/tweets", (req, res) => {
     }
 });
 
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params;
+    try {
+        res.status(200).send(
+            tweets
+                .filter(tweet => tweet.username = username)
+                .map(tweet => ({
+                    ...tweet,
+                    avatar: users.find(user => user.username == tweet.username).avatar
+                }))
+        );
+    } catch (error) {
+        res.status(500).send("Houve um problema interno, por favor tente novamente.");
+    }
+});
