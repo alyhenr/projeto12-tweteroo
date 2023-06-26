77d0ba5a4348b8adfca33a2a28d6ee6654c2a568
Alysson Henrique <alyssonhra99@gmail.com>
2023-06-25 22:49:57 -0300

    change: returning empty array instead of error when asking for tweets of non-existent user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express, { json } from "express";
 import cors from "cors";
-
 import { users, tweets } from "./data.js";
 
 const PORT = 5000;
@@ -13,12 +12,10 @@ app.listen(PORT, () => console.log(`Server on - PORT: ${PORT}`));
 app.post("/sign-up", (req, res) => {
     const { body: { username, avatar } } = req;
     try {
-        if ((!username || !avatar)
-            || (username.toString() !== username || avatar.toString() !== avatar))
+        if (username.toString() !== username || avatar.toString() !== avatar)
             return res.status(400).send("Todos os campos são obrigatórios!");
 
-        const newUser = { username, avatar }
-        users.push(newUser);
+        users.push({ username, avatar });
         res.status(201).send("OK");
     } catch (error) {
         res.status(500).send("Houve um problema no registro, por favor tente novamente.");
@@ -28,8 +25,8 @@ app.post("/sign-up", (req, res) => {
 app.post("/tweets", (req, res) => {
     const { tweet } = req.body;
     const { user } = req.headers;
-    if (!users.find(p => p.username == user)) return res.status(401).send("UNAUTHORIZED");
 
+    if (!users.find(p => p.username == user)) return res.status(401).send("UNAUTHORIZED");
     if (!tweet || tweet.toString() !== tweet) return res.status(400).send("Todos os campos são obrigatórios!");
 
     try {
@@ -44,22 +41,20 @@ app.post("/tweets", (req, res) => {
 app.get("/tweets", (req, res) => {
     const { page } = req.query;
     let start = 0, end = 10;
-
     if (page) {
         if (page >= 1) {
+            page = Math.floor(page);
             end = page * 10;
             start = end - 10;
         } else {
             res.status(400).send("Informe uma página válida!");
         }
     }
-
     try {
         const tweetsInfo = tweets.map(tweet => ({
             ...tweet,
             avatar: users.find(user => user.username == tweet.username).avatar,
         }));
-
         if (tweetsInfo.length > 10) {
             res.status(200).send(tweetsInfo.slice(start, end));
         } else {
@@ -73,14 +68,17 @@ app.get("/tweets", (req, res) => {
 app.get("/tweets/:username", (req, res) => {
     const { username } = req.params;
     try {
-        res.status(200).send(
-            tweets
-                .filter(tweet => tweet.username = username)
+        const user = users.find(user => user.username == username);
+        let tweetInfo = [];
+        if (user) {
+            tweetInfo = tweets
+                .filter(tweet => tweet.username == username)
                 .map(tweet => ({
                     ...tweet,
-                    avatar: users.find(user => user.username == tweet.username).avatar
-                }))
-        );
+                    avatar: users.find(user => user.username == username).avatar
+                }));
+        }
+        res.status(200).send(tweetInfo);
     } catch (error) {
         res.status(500).send("Houve um problema interno, por favor tente novamente.");
     }
